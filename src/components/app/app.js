b79e909285daef3ec4e8f22d61a0514297f6352d
landauleo <bambie7@mail.ru>
2021-01-09 01:30:11 +0300

    add item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,10 +4,13 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from "../item-add-form";
 
 import './app.css'
 
 export default class App extends Component {
+
+    maxId = 100;
 
     state = {
         todoData: [
@@ -19,11 +22,27 @@ export default class App extends Component {
 
     deleteItem = (id) => {
         this.setState(({todoData}) => {
-
             const idx = todoData.findIndex((el) => el.id === id);
 
-            const newArr = [todoData.slice(0, idx), todoData.slice(idx + 1)]
+            const newArr = [
+                ...todoData.slice(0, idx),
+                ...todoData.slice(idx + 1)]
 
+            return {
+                todoData: newArr
+            };
+        });
+    };
+
+    addItem = (text) => {
+        const newItem = {
+            label: text,
+            important: false,
+            id: this.maxId++
+        }
+
+        this.setState(({todoData}) => {
+            const newArr = [...todoData, newItem];
             return {
                 todoData: newArr
             }
@@ -42,6 +61,7 @@ export default class App extends Component {
                 <TodoList
                     todos={this.state.todoData}
                     onDeleted={this.deleteItem}/>
+                <ItemAddForm onItemAdded={this.addItem}/>
             </div>
         );
     }
